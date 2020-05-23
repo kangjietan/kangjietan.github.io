507664d89ce6525f1738a65ff6803b533d5d8921
kangjietan <kangjietan@gmail.com>
2020-05-22 19:05:15 -0700

    Create typed.js strings

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,18 +4,30 @@ $(document).ready(function(e) {
   $toggle = $('.toggle-button');
   var width = $navbar.width();
 
-  toggleLeftNav($win, $navbar, width);
+  resizeLeftNav($win, $navbar, width);
 
   // Resize event for window (browser)
-  $win.resize(() => toggleLeftNav($win, $navbar, width));
+  $win.resize(() => resizeLeftNav($win, $navbar, width));
 
+  // Toggle left navigation
   $toggle.click(() => $navbar.toggleClass("toggle-left"));
+
 });
 
-const toggleLeftNav = ($win , $navbar, width) => {
+const resizeLeftNav = ($win , $navbar, width) => {
   if ($win.width() < 768) {
     $navbar.css({left: `-${width}px`})
   } else {
     $navbar.css({left: '0px'});
   }
 }
+
+const typed = new Typed('#typed', {
+  strings: [
+    'from Daly City.',
+    'a Software Engineer.'
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
